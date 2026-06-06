99ceed394932631079914928e6c344ae9d9534ba
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-06-06 11:17:53 +0530

    A First java program

---
+++ src/basics.java
@@ -0,0 +1,8 @@
+
+void main() {
+    System.out.println("hello world" + " hello shashi");
+    System.out.println(3);
+    System.out.println("4+3");
+    //my name is shashikant
+    System.out.println(4+3);
+}
---
+++ src/classical.java
@@ -0,0 +1,7 @@
+public class classical {
+
+    //entry point
+   public  static void main() {
+       System.out.println("hello shashi");
+    }
+}
