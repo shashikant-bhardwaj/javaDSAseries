20158fc7db6a3bc84fe05a720993f5f7238c0332
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-06-09 21:59:42 +0530

    Loop concept

---
+++ src/loop.java
@@ -0,0 +1,76 @@
+public class loop {
+    static void main() {
+
+        //for loop
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("value of i is:" + i);
+//        }
+//
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("shashi bhardwaj");
+//        }
+//        for (int i = 1; i <= 20; i+=2) {
+//            System.out.println(i);
+//        }
+//        for (int i = 2; i <= 20; i+=2) {
+//            System.out.println(i);
+//        }
+        //NESTED LOOP
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
+//            }
+//        }
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 5) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 5) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 1 || i == 2 || i == 3 || i == 4) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        //WHILE LOOP
+//        int i = 1;
+//        while (i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 1;
+//        while (i <= 3) {
+//          int j = 1;
+//          while (j <= 3) {
+//              System.out.println("i = " + i + " j = " + j);
+//              j++;
+//          }
+//            i++;
+//        }
+
+        //do while loop
+
+        int i =1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+}
