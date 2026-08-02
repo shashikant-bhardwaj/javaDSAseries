d1ec081003bee1a772448e4d50a88c47843649fd
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-02 23:54:57 +0530

    abstraction

--- src/AbstractClass/Main.java
+++ src/AbstractClass/Main.java
@@ -13,9 +13,23 @@ class Sparrow extends Bird {
         System.out.println("I'm eating");
     }
 }
+
+class Crow extends Bird {
+    void fly(){
+        System.out.println("crow flying");
+    }
+
+    void eat(){
+        System.out.println("crow eating");
+    }
+}
 public class Main {
     public static void main() {
         Bird b = new Sparrow();
+        b.fly();
+        b.eat();
+
+        b = new Crow();
         b.fly();
         b.eat();
     }
