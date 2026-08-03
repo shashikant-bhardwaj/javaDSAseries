24e8ec81f7bab34fd4f7db09ff0b039510ae0542
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-04 00:48:44 +0530

    interface in java

--- src/AbstractClass/Main.java
+++ src/AbstractClass/Main.java
@@ -1,36 +1,101 @@
+//package AbstractClass;
+//
+//abstract class Bird{
+//     abstract void fly();
+//     abstract void eat();
+//     public void sleep(){
+//         System.out.println("bird sleeping");
+//     }
+//        }
+//class Sparrow extends Bird {
+//    void fly(){
+//        System.out.println("I'm flying");
+//    }
+//
+//    void eat(){
+//        System.out.println("I'm eating");
+//    }
+//}
+//
+//class Crow extends Bird {
+//    void fly(){
+//        System.out.println("crow flying");
+//    }
+//
+//    void eat(){
+//        System.out.println("crow eating");
+//    }
+//}
+//public class Main {
+//    public static void doBirdStuff(Bird b){
+//        b.fly();
+//        b.fly();
+//        b.sleep();
+//    }
+//    public static void main() {
+//        Bird b = new Sparrow();
+//        b.fly();
+//        b.eat();
+//
+//        b = new Crow();
+//        b.fly();
+//        b.eat();
+//        doBirdStuff(new Sparrow());
+//        doBirdStuff(new Crow());
+//    }
+//}
+
+
 package AbstractClass;
 
-abstract class Bird{
-     abstract void fly();
-     abstract void eat();
-        }
-class Sparrow extends Bird {
-    void fly(){
-        System.out.println("I'm flying");
-    }
-
-    void eat(){
-        System.out.println("I'm eating");
+interface Bird{
+    void fly();
+    void eat();
+    default void sleep(){
+        System.out.println("bird sleeping");
     }
 }
 
-class Crow extends Bird {
-    void fly(){
+interface Walk{
+    void walking();
+}
+class Sparrow implements Bird, Walk {
+    public void fly(){
+        System.out.println("sparrow flying");
+    }
+
+    public void eat(){
+        System.out.println("sparrow eating");
+    }
+    public void walking(){
+        System.out.println("sparrow walking");
+    }
+}
+
+class Crow implements Bird {
+    public void fly(){
         System.out.println("crow flying");
     }
 
-    void eat(){
+    public void eat(){
         System.out.println("crow eating");
     }
 }
 public class Main {
+    public static void doBirdStuff(Bird b){
+        b.fly();
+        b.fly();
+        b.sleep();
+    }
     public static void main() {
         Bird b = new Sparrow();
-        b.fly();
-        b.eat();
-
-        b = new Crow();
-        b.fly();
-        b.eat();
+//        b.fly();
+//        b.eat();
+//
+//        b = new Crow();
+//        b.fly();
+//        b.eat();
+        doBirdStuff(new Sparrow());
+        doBirdStuff(new Crow());
     }
 }
